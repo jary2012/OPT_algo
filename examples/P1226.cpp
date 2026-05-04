20f954d04cc21fb8361cc020d72b72596b9ab999
jary2012 <3805277423@qq.com>
2026-05-04 13:37:25 +0800

    快速幂

--- examples/P1226.cpp
+++ examples/P1226.cpp
@@ -1,12 +1,11 @@
 #include <bits/stdc++.h>
-#include "./math/quick_pow/iterative/quick_pow_iterative.hpp"
-#include "./math/quick_pow/recursive/quick_pow_recursive.hpp"
+#include "./math/quick_pow/iterative/quick_pow.hpp"
+#include "./math/quick_pow/recursive/quick_pow.hpp"
 using namespace std;
 using namespace opt_algo;
 
 int main()
 {
-    std::cout << "迭代版 2^10 mod 1e9+7 = " << quick_pow_iterative(2, 10, 1000000007) << "\n";
-    std::cout << "递归版 2^60 mod 1e9+7 = " << quick_pow_recursive(2LL, 60, 1000000007LL) << "\n";
+    cout << "迭代版 2^10 mod 1e9+7 = " << quick_pow_iterative(2, 10, 1000000007) << "\n";
+    cout << "递归版 2^60 mod 1e9+7 = " << quick_pow_recursive(2LL, 60LL, 1000000007LL) << "\n";
     return 0;
-}
